c893c6a5974be00e2e496fcd7325f9e1da1f404e
nandakishore0434 <nandakishore0434@gmail.com>
2026-08-14 09:39:27 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 alert("welcome to nriiit learning management nandu");
 let handing= document.getElementById("welcome")
 heading.style.color="red";
+
+if(registerForm){
+    registerForm.addEventListener("SUBMIT",function)
+
+let email=document.getElementById("email").Value;
+let password=document.getElementById("password").value;
+let gender=" ";
+if(male){
+    gender="male";
+}else if(femail){
+    gender="female";
+}
+if(name==""|| email==""||password==""|| date==""|| gender==""||course==""){
+    alert("plz fill all the fields");
+}else{
+    alert("registration sucessful");
+}
+}
+let loginForm=document.getElementById("loginForm")
+if(loginForm){
+    loginForm.addEventListener("submit",function(event))
+    {
+        event.preventDefault();
+        console.log(event);
+        let email=document.getElementById("loginemail").value;
+        let password=document.getElementById("loginpassword").value;
+        if (email==""||password==""){
+            alert("plz fill all the fields");
+        }else{
+            
+        }
+    }
+}
